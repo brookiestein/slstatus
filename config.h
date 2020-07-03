83330ae2d83fdadc07830c015373353272ea6bce
brookiestein <brookie1@protonmail.com>
2020-07-03 10:42:45 -0400

    Adding a new feature.
    Adding a command to view your city's current weather.

--- config.h
+++ config.h
@@ -62,11 +62,16 @@ static const char unknown_str[] = "n/a";
  * wifi_essid          WiFi ESSID                      interface name (wlan0)
  */
 
-static const char command[] = "a=$(amixer get Master | tail -n 1 | cut -d ' '\
-                               -f 8); if [ $a = '[on]' ]; then printf \"$(amixer \
+static const char vol[]         = "a=$(amixer get Master | tail -n 1 | cut -d ' '\
+                                -f 8); if [ $a = '[on]' ]; then printf \"$(amixer \
                                 get Master | tail -n 1 | cut -d ' ' -f 7 | \
-                               grep -Po '\\[\\K[^%]*')%%\"; \
-                               else printf 'Muted'; fi";
+                                grep -Po '\\[\\K[^%]*')%%\"; \
+                                else printf 'Muted'; fi";
+
+/* Put your city's name after the -l parameter. */
+static const char weather[]     = "a=`ansiweather -l 'Your city name here'\
+                                -a false -s false -i false -w false -h false \
+                                -p false | cut -d ':' -f 2`; a=${a:1:${#a}-1}; echo -en $a";
 
 static const char display_light[] = "xbacklight -get";
 static const char bat[] = "BAT0";
@@ -81,7 +86,8 @@ static const struct arg args[] = {
         { run_command,  ":%s%%|",      display_light }, /* Display light. */
         { battery_perc, ":%s%% ",      bat }, /* Battery percent. */
         { battery_state,"%s|",          bat }, /* Battery state. */
-        { run_command,  ":%s|",        command }, /* Volume percent. */
+        { run_command,  ":%s|",        vol }, /* Volume percent. */
         { keymap,       "⌨:%s|",        NULL }, /* Keyboard layout */
-        { datetime,     ":%s|",        format_datetime }, /* Date time with this format: Day YYYY-MM-DD 18:00:00 */
+        { datetime,     ":%s|",        format_datetime }, /* Date time with this format: Day name YYYY-MM-DD 18:00:00 */
+        { run_command,  "☀:%s|",        weather } /* Your city's weather. */
 };
